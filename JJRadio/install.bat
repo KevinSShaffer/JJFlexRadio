06099e89fa1a4c726dce1f68a80bf1ea1c296dcf
Kevin Shaffer <kevinsshaffer@gmail.com>
2024-05-04 15:29:13 -0600

    Add installer dependencies

--- JJRadio/install.bat
+++ JJRadio/install.bat
@@ -7,9 +7,9 @@ echo %pgm%
 cd %1
 rem modify the install script
 echo "modifying install script"
-type "install template.nsi" | "C:\Program Files (x86)\GnuWin32\bin\sed" "s/MYPGM/%pgm%/g" >install.nsi
+type "install template.nsi" | "GnuWin32\bin\sed" "s/MYPGM/%pgm%/g" >install.nsi
 rem create the delete list.
 echo "creating delete list"
-dir /b bin\release | "C:\Program Files (x86)\GnuWin32\bin\sed" -f src.sed >deleteList.txt
+dir /b bin\release | "GnuWin32\bin\sed" -f src.sed >deleteList.txt
 echo "creating OutFile from the install.nsi script"
-"C:\Program Files (x86)\NSIS\makensis" install.nsi
+"NSIS\makensis" install.nsi
